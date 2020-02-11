67e4430e07d6b762cdca605959dff8bf7c46f9a7
Ubuntu <ubuntu@ip-172-31-82-203.ec2.internal>
2020-02-11 02:11:15 +0000

    init commit

---
+++ MapleLogs.java
@@ -0,0 +1,21 @@
+/**
+* This program calculates the number of logs
+* that a logging truck can carry with user input.
+*
+* @author  Christina Ngwa
+* @version 1.0
+* @since   2020-02-10 
+*/
+
+public class MapleLogs {
+  public static void main(String []args) {
+    
+    // variables
+    int logWeight = 20;
+    int truckCapacity = 1100;
+    double numOfLogs;
+    double logLength;
+    
+    
+  }
+}
