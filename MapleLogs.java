c28f0c4d03ce5d4fccc87c18ef1fc207e431306e
Ubuntu <ubuntu@ip-172-31-82-203.ec2.internal>
2020-02-11 17:48:27 +0000

    commit

--- MapleLogs.java
+++ MapleLogs.java
@@ -7,6 +7,8 @@
 * @since   2020-02-10 
 */
 
+import java.util.Scanner;
+
 public class MapleLogs {
   public static void main(String []args) {
     
@@ -15,7 +17,15 @@ public class MapleLogs {
     int truckCapacity = 1100;
     double numOfLogs;
     double logLength;
+  
+    System.out.println("Welcome to the Maple Log delivery.\nThe only valid choices for cut logs are 0.25m, 0.5m or 1m.\n");
     
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.print("Enter the length of the logs in meters: ");
+    
+    logLength = myObj.nextDouble();  // Read user input
+    numOfLogs = truckCapacity/ (logWeight*logLength);
+    System.out.println("The truck can carry " + numOfLogs + " logs that are " + logLength + " meter(s) long.");
     
   }
 }
