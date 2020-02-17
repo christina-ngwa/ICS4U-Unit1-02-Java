e1d9912cb4915d4c2778364eeffc995e88268fb3
Ubuntu <ubuntu@ip-172-31-82-203.ec2.internal>
2020-02-17 00:59:35 +0000

    comments

--- MapleLogs.java
+++ MapleLogs.java
@@ -1,31 +1,46 @@
+import java.util.Scanner;
+
 /**
 * This program calculates the number of logs
 * that a logging truck can carry with user input.
 *
 * @author  Christina Ngwa
 * @version 1.0
-* @since   2020-02-10 
+* @since   2020-02-11
 */
 
-import java.util.Scanner;
-
 public class MapleLogs {
-  public static void main(String []args) {
-    
-    // variables
-    int logWeight = 20;
-    int truckCapacity = 1100;
-    double numOfLogs;
-    double logLength;
+  /**
+  * This program calculates the number of logs
+  * that a logging truck can carry with user input.
+  *
+  * @author  Christina Ngwa
+  * @version 1.0
+  * @since   2020-02-11
+  */
   
-    System.out.println("Welcome to the Maple Log delivery.\nThe only valid choices for cut logs are 0.25m, 0.5m or 1m.\n");
+  public static void main(String []args) {
+    // variables
     
+    final int logWeight = 20;
+    final int truckCapacity = 1100;
+    final double numOfLogs;
+    final double logLength;
+    
+    // output
+    System.out.println("Welcome to the Maple Log delivery.");
+    System.out.println("The valid choices for cut logs are 0.25m, 0.5m or 1m.");
+  
+    // input
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.print("Enter the length of the logs in meters: ");
-    
+    System.out.print("\nEnter the length of the logs in meters: ");
+  
+    // process
     logLength = myObj.nextDouble();  // Read user input
-    numOfLogs = truckCapacity/ (logWeight*logLength);
-    System.out.println("The truck can carry " + numOfLogs + " logs that are " + logLength + " meter(s) long.");
-    
+    numOfLogs = truckCapacity / (logWeight * logLength);
+
+    System.out.print("\nThe truck can carry " + numOfLogs + " logs that are " 
+                     + logLength + " meter(s) long.");
+
   }
 }
